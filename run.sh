558a7128b6ba43187cabf5df1062bd204dd28c57
sasanquaa <arestvn@gmail.com>
2022-10-14 20:04:26 +0700

    One day I will go mad because I cant understand how to setup a TTB for aarch64

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 ./build.sh &&
-qemu-system-arm -nographic -M virt -kernel Build/Boot/Main &&
+qemu-system-aarch64 -nographic -M virt -cpu cortex-a72 -kernel Build/Boot/Main &&
 clear &&
-arm-none-eabi-objdump -Shsd Build/Boot/Main
+/opt/gcc-aarch64-none-elf/bin/aarch64-none-elf-objdump -Shsd Build/Boot/Main
